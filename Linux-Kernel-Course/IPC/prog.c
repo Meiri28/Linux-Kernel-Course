c4d532bf104a8df00782fd1e82752bfd1d3427b5
jonathan_meiri <yonn200@gmail.com>
2022-04-02 15:29:45 +0300

    running wc and get result with pipe

--- Linux-Kernel-Course/IPC/prog.c
+++ Linux-Kernel-Course/IPC/prog.c
@@ -7,23 +7,49 @@
 #include <errno.h>
 #include <string.h>
 
-#define SELF "IPC.out"
+#define SELF "./a.out"
 #define MAX_DIRECTORY_NAME_LENGTH 255
 
-int create_child(char* argv)
+int read_int_from_fd(int fd) {
+	int result = 0;
+	char current;
+	while (read(fd, &current, 1) && current >= '0' && current <= '9') {
+		result *= 10;
+		result += current - '0';
+	}
+
+	return result;
+}
+
+int create_child(char* path)
 {
+	struct stat path_stat;
+	stat(path, &path_stat);
+	int pfds[2];
+	pipe(pfds);
 	int pid = fork();
 	//check if child
 	if (pid == 0) {
-		execl(SELF, SELF, argv);
+		if (S_ISREG(path_stat.st_mode)) {
+			close(1);
+			dup(pfds[1]);
+			
+			execl("/bin/wc", "/bin/wc", "-w", path);
+		}
+		else if (S_ISDIR(path_stat.st_mode))
+			execl(SELF, SELF, path);
+		
 		printf("failed %d\n", errno);
+		exit(1);
 	}
+
+	printf("number of word %d \n", read_int_from_fd(pfds[0]));
 
 	return pid;
 }
 
 int main(int argc, char * argv[]) {
-	//printf("%s %s\n", argv[0], argv[1]);
+	printf("%s %s\n", argv[0], argv[1]);
 	struct stat file_stat;
 	stat(argv[1], &file_stat);
 	
@@ -40,8 +66,8 @@ int main(int argc, char * argv[]) {
 		char* full_path = calloc(strlen(argv[1]) + MAX_DIRECTORY_NAME_LENGTH, sizeof(char));
 		while (dp != NULL) {
 			strcpy(full_path, argv[1]);
-			strcat(full_path, dp->d_name);
 			strcat(full_path, "/");
+			strcat(full_path, dp->d_name);
 			create_child(full_path);
 			
 			dp = readdir(current_dir);
