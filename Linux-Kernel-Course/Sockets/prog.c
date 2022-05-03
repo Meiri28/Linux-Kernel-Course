3894d989dd0270ed8edec9a373f93c16d24c487d
jonathan_meiri <yonn200@gmail.com>
2022-05-03 18:34:30 +0300

    finished

--- Linux-Kernel-Course/Sockets/prog.c
+++ Linux-Kernel-Course/Sockets/prog.c
@@ -9,6 +9,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fcntl.h>
 
 #define MAX_HTTP_HEADER_SIZE 4096
 
@@ -28,19 +31,20 @@ enum request_status
 	wait_for_send,
 	wait_for_rcv_header,
 	wait_for_rcv_body,
+	finish,
 };
 
 struct MyStruct
 {
 	char* request;
 	char* file_path;
+	int dest_fd;
 	int fd;
 	enum request_status status;
 	char headr[MAX_HTTP_HEADER_SIZE];
 	int byte_upload;
 	int byte_download_to_header;
 	int bytes_download_to_body;
-	char* body;
 };
 
 int running;
@@ -80,7 +84,10 @@ int connect_http_with_domain(char* domain)
 
 void stop(int num) 
 {
+	if (num >= number_of_requests)
+		return;
 	close(requests[num].fd);
+	close(requests[num].dest_fd);
 	free(requests[num].file_path);
 	free(requests[num].request);
 	while (num < number_of_requests - 1)
@@ -130,6 +137,7 @@ int get_fd_from_request(char* request)
 	char* domain = get_domain_from_requests(request);
 	int f = connect_http_with_domain(domain);
 	free(domain);
+	fcntl(f, F_SETFL, O_NONBLOCK);
 
 	return f;
 }
@@ -143,15 +151,17 @@ void get_command() {
 	} else if (strncmp(line, "stop", sizeof(char) * strlen("stop")) == 0) {
 		stop(atoi(line + strlen("stop") + 1));
 	} else if (strncmp(line, "start", sizeof(char) * strlen("start")) == 0) {
-		requests = realloc(requests, sizeof(number_of_requests + 1));
+		requests = realloc(requests, sizeof(struct MyStruct) * (number_of_requests + 1));
 		number_of_requests++;
 		char* url = strstr(line, " ") + 1;
 		char* endurl = strstr(url, " ");
 		requests[number_of_requests - 1].request = malloc(sizeof(char) * (int)(endurl - url + 1));
-		strncat(requests[number_of_requests - 1].request, url, endurl - url);
+		strncpy(requests[number_of_requests - 1].request, url, endurl - url);
 		requests[number_of_requests - 1].request[endurl - url] = '\0';
 		requests[number_of_requests - 1].file_path = malloc(sizeof(char) * strlen(endurl + 1));
-		strcat(requests[number_of_requests - 1].file_path, endurl + 1);
+		strcpy(requests[number_of_requests - 1].file_path, endurl + 1);
+		requests[number_of_requests - 1].file_path[strlen(requests[number_of_requests - 1].file_path) - 1] = '\0';
+		requests[number_of_requests - 1].dest_fd = open(requests[number_of_requests - 1].file_path, O_CREAT| O_WRONLY, S_IRWXU);		
 		requests[number_of_requests - 1].bytes_download_to_body = 0;
 		requests[number_of_requests - 1].byte_download_to_header = 0;
 		requests[number_of_requests - 1].byte_upload = 0;
@@ -171,7 +181,7 @@ void sent_header(int num)
 	char* request = create_get_request(domain, path);
 	free(domain);
 	free(path);
-
+	
 	requests[num].byte_upload += send(requests[num].fd, request + requests[num].byte_upload, strlen(request + requests[num].byte_upload), 0);
 	if (requests[num].byte_upload == strlen(request))
 	{
@@ -180,20 +190,42 @@ void sent_header(int num)
 	free(request);
 }
 
+int body_size(char* header) 
+{
+	char* a = strstr(header, "Content-Length: ");
+
+	return atoi(a + (sizeof(char) * strlen("Content-Length: ")));;
+}
+
 void receve_http(int num) 
 {
-	char header[4096];
-	if(recv(requests[num].fd,&header, 4096,0) == -1)
-		printf("recv failed with errno %d\n", errno);
-	
-	printf("%s", header);
+	if (requests[num].status == wait_for_rcv_header) {
+		requests[num].byte_download_to_header += recv(requests[num].fd, &requests[num].headr + requests[num].byte_download_to_header, 4096 - requests[num].byte_download_to_header, 0);
+		char* end_of_header = strstr(requests[num].headr, "\r\n\r\n");
+		if (end_of_header != NULL)
+		{
+			*end_of_header = '\0';
+			requests[num].status = wait_for_rcv_body;
+			char* start_of_body = end_of_header + sizeof(char) * strlen("\r\n\r\n");
+			int a = write(requests[num].dest_fd, start_of_body, strlen(start_of_body));
+			requests[num].bytes_download_to_body += strlen(start_of_body);
+		}
+	} else {
+		char body[500];
+		int byte_downloaded = recv(requests[num].fd, body, 500, 0);
+		requests[num].bytes_download_to_body += byte_downloaded;
+		write(requests[num].dest_fd, body, byte_downloaded);
+	}
+	if (requests[num].status == wait_for_rcv_body && body_size(requests[num].headr) <= requests[num].bytes_download_to_body){
+		requests[num].status = finish;
+	}
 }
 
 int main() {
 	running = 1;
 	number_of_requests = 0;
+	requests = NULL;
 	while (running) {
-		//int *available_fd = malloc(sizeof(int) * (number_of_requests + 1));
 		struct pollfd* poll_request = malloc(sizeof(struct pollfd) * (number_of_requests + 1));
 		for (int i = 0; i < number_of_requests; i++) {
 			poll_request[i].fd = requests[i].fd;
@@ -215,21 +247,20 @@ int main() {
 			get_command();
 			
 		}
-			
 		free(poll_request);
-	}
-	
-	//int f = connect_http_with_domain("google.com");
-	//char* resuest = create_get_request("google.com", "/index.html");
-	//if (send(f, resuest, strlen(resuest), 0) == -1)
-	//	printf("send failed with errno %d\n", errno);
-	//free(resuest);
-	//char header[4096];
-	//if(recv(f,&header, 4096,0) == -1)
-	//	printf("recv failed with errno %d\n", errno);
-	//
-	//printf("%s", header);
+		int temp = 0;
+		while (temp < number_of_requests) {
+			if (requests[temp].status == finish) {
+				printf("download from %s to %s finished\n", requests[temp].request, requests[temp].file_path);
+				stop(temp);
+			}
+			else
+				temp++;
+		}
+		{
 
+		}
+	}
 
 	return 0;
 }
